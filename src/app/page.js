4a988c3c57bd0b8383f964b0681a0217b873da7d
tapfer-abdullah <tapfer.abdullah@gmail.com>
2024-01-03 16:44:44 +0600

    updating

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
-
 import Banner from '@/Components/Pages/Home/Banner/Banner';
 import HomePolicy from '@/Components/Pages/Home/HomePolicy/HomePolicy';
 import NewArrival from '@/Components/Pages/Home/NewArrival/NewArrival';
 import Products from '@/Components/Pages/Home/Products/Products';
+import 'core-js'; //for self
 
 
 export default function Home() {
